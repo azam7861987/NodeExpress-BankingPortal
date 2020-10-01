e94635083649b7005305bbad9dbfbcd2b9575f58
Mohammed Azam <azam.mohammed@compucom.com>
2020-10-02 00:58:13 +0530

    code submitted module 2

--- src/app.js
+++ src/app.js
@@ -9,6 +9,32 @@ app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', (req, res) => res.render('index', { title: 'Index' }));
+let accountData = fs.readFileSync(path.join(__dirname, '/json')+'/accounts.json','utf8');
+let userData = fs.readFileSync(path.join(__dirname, '/json')+'/users.json','utf8');
+const accounts = JSON.parse(accountData);
+const users = JSON.parse(userData);
+app.get('/', (req, res) => {  
+    res.render('index', { title: 'Account Summary', accounts:accounts })
+});
 
+app.get('/savings', (req, res) => {
+    let accountData = fs.readFileSync(path.join(__dirname, '/json')+'/accounts.json','utf8');
+    const accounts = JSON.parse(accountData);
+    res.render('account', { title: 'Account Summary', account:accounts.savings })
+});
+
+app.get('/checking', (req, res) => {
+    res.render('account', { title: 'Account Summary', account:accounts.checking })
+});
+
+app.get('/credit', (req, res) => {
+    res.render('account', { title: 'Account Summary', account:accounts.credit })
+});
+
+app.get('/profile',(req,res)=>{
+    res.render("profile",{
+        title:"Profile",
+        user: users[0]       
+    })
+})
 app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
